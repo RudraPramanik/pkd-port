f3067d9b18a1e443287c0d178c732e6c231f23f4
rudra pramanik <rudrahimangshoo2020@gmail.com>
2023-10-11 02:22:08 +0600

    change

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ export const headerData = {
   name: 'Prosunto Kumar Biswas',
   title: 'Hello! I am',
   desciption: "Hey I'm 	Prosunto, Self-motivated & passionate Engineer ",
-  image: 'https://ibb.co/MC4Y0RV',
+  image: '',
   imagebw: '',
   resumePdf:
     'https://drive.google.com/drive/folders/1DdjIYWSQ5Zrg0_k5nA4xoWNDBQZzfTwq?usp=sharing',
